f04f02b6f6e3e63d8b3023415bb7701dbed4b971
supermoonie <wangchao_moonie@163.com>
2021-08-15 19:42:55 +0800

    jvm

--- src/test/java/com/github/supermoonie/console4j/router/JvmRouterTest.java
+++ src/test/java/com/github/supermoonie/console4j/router/JvmRouterTest.java
@@ -3,8 +3,14 @@ package com.github.supermoonie.console4j.router;
 import com.sun.tools.attach.VirtualMachine;
 import com.sun.tools.attach.VirtualMachineDescriptor;
 import org.junit.Test;
+import sun.management.ConnectorAddressLink;
 
+import javax.management.remote.JMXConnector;
+import javax.management.remote.JMXConnectorFactory;
+import javax.management.remote.JMXServiceURL;
+import java.io.IOException;
 import java.util.List;
+import java.util.Properties;
 
 /**
  * @author super_w
@@ -20,6 +26,21 @@ public class JvmRouterTest {
             String name = vm.displayName();
             System.out.println("id: " + id + ", name: " + name);
         }
+    }
+
+    @Test
+    public void attachLocalJvm() throws Exception {
+        int pid = 15601;
+        String address = ConnectorAddressLink.importFrom(pid);
+        if (null == address) {
+            VirtualMachine virtualMachine = VirtualMachine.attach(Integer.toString(pid));
+            String url = virtualMachine.startLocalManagementAgent();
+            Properties properties = virtualMachine.getAgentProperties();
+            address = (String) properties.get("com.sun.management.jmxremote.localConnectorAddress");
+        }
+        JMXServiceURL jmxServiceURL = new JMXServiceURL(address);
+        JMXConnector jmxConnector = JMXConnectorFactory.connect(jmxServiceURL, null);
+
     }
 
 }
